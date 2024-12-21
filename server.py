5dd7d68114211cb24b50e83c5e80b24be734a470
Zannanacklepoc <eddy.leah@yahoo.com>
2024-12-21 15:57:13 -0500

    fixed

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
 from flask_app import app
 from flask_app.controller import main
-import os
 
-if __name__ == "__main__":
-    # Heroku requires app to run on host 0.0.0.0 and a dynamic port from the environment
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+
+
